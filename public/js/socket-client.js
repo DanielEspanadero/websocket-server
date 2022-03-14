f2119e343c209a87a1eabf167b1b93a4a679f11f
Daniel Españadero <developer@daniel-espanadero.com>
2022-03-14 14:27:10 +0100

    connect and disconnect socket

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,8 +1,26 @@
 (() => {
 window.addEventListener('load', ()=>{
 
+
+    // Referencias del HTML
+    const lblOnline = document.querySelector('#lblOnline')
+    const lblOffline = document.querySelector('#lblOffline')
+
+
+
     const socket = io();
 
+    socket.on('connect', ()=>{
+        console.log('conectado');
+        lblOffline.style.display = 'none';
+        lblOnline.style.display = 'inline';
+    });
+
+    socket.on('disconnect', ()=>{
+        console.log('Desconectado del servidor');
+        lblOnline.style.display = 'none';
+        lblOffline.style.display = 'inline';
+    });
 });
 })();
 
